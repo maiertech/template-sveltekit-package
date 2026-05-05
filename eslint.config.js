766f4fc19fb230f442f46b5e5d1fe862935c11ca
Thilo Maier <github@maier.asia>
2026-05-05 20:25:01 +0200

    Update project config (#106)
    * Update README.md
    
    * Update project config
    
    * Fix svelte-check error

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
+import prettier from 'eslint-config-prettier';
+import path from 'node:path';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
-import prettier from 'eslint-config-prettier';
 import svelte from 'eslint-plugin-svelte';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
-import path from 'node:path';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
 
@@ -13,10 +13,10 @@ const gitignorePath = path.resolve(import.meta.dirname, '.gitignore');
 export default defineConfig(
 	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
-	...ts.configs.recommended,
-	...svelte.configs.recommended,
+	ts.configs.recommended,
+	svelte.configs.recommended,
 	prettier,
-	...svelte.configs.prettier,
+	svelte.configs.prettier,
 	{
 		languageOptions: { globals: { ...globals.browser, ...globals.node } },
 		rules: {
@@ -35,5 +35,10 @@ export default defineConfig(
 				svelteConfig
 			}
 		}
+	},
+	{
+		// Override or add rule settings here, such as:
+		// 'svelte/button-has-type': 'error'
+		rules: {}
 	}
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import devtoolsJson from 'vite-plugin-devtools-json';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()],
+	plugins: [sveltekit()],
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
